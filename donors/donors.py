8ba052b508d88462c5061ddfb60d1a845757590e
Vivek Sridhar <vivsridh2@gmail.com>
2019-06-09 04:11:41 +0530

    formatting at donors main project

--- donors/donors.py
+++ donors/donors.py
@@ -11,7 +11,6 @@ from donors.getprojects import getprojectdetails
 
 @click.command()
 @click.option("--user","-u",help='donors cli project users operations: Create Users or List Users',type=click.Choice(['create', 'list']))
-
 @click.option("--getprojects","-gp",help='list all projects based on user id(Row-ID from Table), get user id details from --user list',type=int)
 
 
