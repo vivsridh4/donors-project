5557fbca738967c2d98bf132b067739508dd6e47
Vivek Sridhar <vivsridh2@gmail.com>
2020-10-16 18:00:09 +0530

    demo again

--- donors/donors.py
+++ donors/donors.py
@@ -21,7 +21,7 @@ def main(user,getprojects):
             users_ops(user)
         else:
             print("Please add valid keys in config.ini")
-            print("nice to see devops at microsoft reactor")
+            print("This is CD vs CD!")
     if getprojects:
         
         #Verify if config file has been updated with keys.
