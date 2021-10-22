cb596c94121d564706ad4f6c96c90a2204efca51
Vivek Sridhar <32955433+vivsridh4@users.noreply.github.com>
2021-10-22 17:12:01 +0530

    Update donors.py

--- donors/donors.py
+++ donors/donors.py
@@ -21,7 +21,7 @@ def main(user,getprojects):
             users_ops(user)
         else:
             print("Please add valid keys in config.ini")
-            print("This is CD vs CD; testing this code live")
+            print("This is CD vs CD; testing this code live!")
     if getprojects:
         
         #Verify if config file has been updated with keys.
